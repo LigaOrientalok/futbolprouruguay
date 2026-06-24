6b27dd66258448a63aad52343d0e25bf572688ad
LigaOrientalok <bolso2340@gmail.com>
2026-06-24 17:37:04 -0300

    chore: update config files (eslint, next.js, dependencies)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next"
-import path from "path"
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: path.resolve(__dirname),
-  },
+  reactCompiler: true,
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "utfs.io" },
